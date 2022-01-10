d935916a10558d982730adcda37fecd57a4c1738
Janick Braun <nasebuch@gmail.com>
2022-01-10 21:52:40 +0100

    web

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,9 +3,9 @@ import Unity, { UnityContext } from "react-unity-webgl"
 
 const unityContext = new UnityContext({
   loaderUrl: "build/HTML.loader.js",
-  dataUrl: "build/HTML.data.gz",
-  frameworkUrl: "build/HTML.framework.js.gz",
-  codeUrl: "build/HTML.wasm.gz",
+  dataUrl: "build/HTML.data",
+  frameworkUrl: "build/HTML.framework.js",
+  codeUrl: "build/HTML.wasm",
 })
 
 export default function Game() {
