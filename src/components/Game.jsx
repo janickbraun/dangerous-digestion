af1a719e7d89ad238039d4e9782bbd3180317c48
Janick Braun <nasebuch@gmail.com>
2022-01-10 23:21:43 +0100

    web

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,7 +3,27 @@ import React from "react"
 export default function Game() {
   return (
     <div>
-      <h1>Coming soon</h1>
+      <center>
+        <iframe title="game" src="https://itch.io/embed/1348943" height="167" width="55%" frameborder="0">
+          <a href="https://karottenkopf.itch.io/dangerous-digestion">Dangerous Digestion by Janick</a>
+        </iframe>
+      </center>
+      <br />
+      <center>
+        <iframe
+          id="webgl_iframe"
+          frameborder="0"
+          allow="autoplay; fullscreen; vr"
+          allowfullscreen=""
+          allowvr=""
+          mozallowfullscreen="true"
+          src="https://play.unity3dusercontent.com/webgl/5fb2bfe5-ed44-4f03-bedd-a455313715c1?screenshot=false&embedType=embed"
+          width="55%"
+          height="640"
+          onmousewheel=""
+          webkitallowfullscreen="true"
+        ></iframe>
+      </center>
     </div>
   )
 }
