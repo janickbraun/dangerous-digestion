91dd2b108e49074d1a2096bb3406834054e02320
Janick Braun <nasebuch@gmail.com>
2022-01-10 21:58:22 +0100

    build

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,9 @@
 import React from "react"
-import Unity, { UnityContext } from "react-unity-webgl"
-
-const unityContext = new UnityContext({
-  loaderUrl: "build/HTML.loader.js",
-  dataUrl: "build/HTML.data",
-  frameworkUrl: "build/HTML.framework.js",
-  codeUrl: "build/HTML.wasm",
-})
 
 export default function Game() {
   return (
     <div>
-      <Unity unityContext={unityContext} />
+      <h1>Coming soon</h1>
     </div>
   )
 }
